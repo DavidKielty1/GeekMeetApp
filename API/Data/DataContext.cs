1ca8517e209270332ab11e056554bfa86825c059
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2023-10-08 17:24:02 +0100

    Added User controller logic

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,4 +1,4 @@
-﻿using API.Entities;
+﻿﻿using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
@@ -8,5 +8,6 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions options) : base(options)
     {
     }
-    public DbSet<AppUser> Users{ get; set; }
+
+    public DbSet<AppUser> Users { get; set; }
 }
